c5861897727de1621a5dff1608d211e8ee06ac3d
quentin-marc <quentinmarc81@gmail.com>
2022-04-12 14:21:29 +0200

    import dico nettoye

--- dictionnaireInitial/scriptNettoyerDonnees.py
+++ dictionnaireInitial/scriptNettoyerDonnees.py
@@ -76,6 +76,7 @@ def nettoyerAdverbes():
 
         # parcourt tout le csv pour trouver les adverbes
         for row in csv_reader:
+            if " " not in row[1]:
                 listeAdv.append(row[1])
 
         # sauvegarde des resultats tries dans un json
@@ -87,6 +88,41 @@ def nettoyerAdverbes():
 
         # affichage du resultat
         print(f' {len(listeAdv)} adverbes extraits.')
+
+
+# extrait et sauvegarde les adjectifs en supprimant ceux avec des espaces et les formes non nominales (feminin, pluriel)
+def nettoyerAdjectifs():
+
+    # ouverture du csv
+    with open('adj.csv') as csv_adj:
+
+       # initialisation des variables
+        csv_reader = csv.reader(csv_adj, delimiter=',')
+        listeAdj = []
+        listeAdjASupprimer = []
+
+        for row in csv_reader:
+            # recuperation de tous les noms qui ne possedent pas d'espaces, qui ne sont pas tagues et dont la taille est superieure a 1 caractere
+            if " " not in row[1] and 'plural' not in row[2] and 'feminine' not in row[2] and len(row[1]) > 1:
+                listeAdj.append(row[1])
+            # recuperation de tous les noms tagues
+            if " " not in row[1] and ('plural' in row[2] or 'feminine' in row[2]) and len(row[1]) > 1:
+                listeAdjASupprimer.append(row[1])
+
+        # soustraction des mots tagues au jeu de donnees a conserver pour garder que les formes sg, et masculin
+        for nom in listeAdjASupprimer:
+            if nom in listeAdj:
+                listeAdj.remove(nom)
+
+        # sauvegarde des resultats tries dans un json
+        listeAdj.sort()
+        jsonString = json.dumps(listeAdj)
+        jsonFile = open("../dictionnaireUtilisable/adjectifs.json", "w")
+        jsonFile.write(jsonString)
+        jsonFile.close()
+
+        # affichage du resultat
+        print(f' {len(listeAdj)} adjectifs extraits.')
 
 # lancement du nettoyage desire
 nettoyerAdverbes()
