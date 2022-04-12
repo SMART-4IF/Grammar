5509c6c5171cab02aa755837e24e2d4cdf1b284e
quentin-marc <quentinmarc81@gmail.com>
2022-04-12 13:53:11 +0200

    start dictionnaire nettoyage

--- dictionnaireInitial/scriptNettoyerDonnees.py
+++ dictionnaireInitial/scriptNettoyerDonnees.py
@@ -3,7 +3,7 @@
 import csv
 import json
 
-# extrait et sauvegarde les formes infinitives de la base de données des verbes
+# extrait et sauvegarde les formes infinitives de la base de donnees des verbes
 def nettoyerVerbes():
 
     # ouverture du csv
@@ -18,15 +18,75 @@ def nettoyerVerbes():
             if "'infinitive'" in row[2]:
                 listeVerbes.append(row[1])
 
-        # sauvegarde des résultats triés dans un json
+        # sauvegarde des resultats tries dans un json
         listeVerbes.sort()
         jsonString = json.dumps(listeVerbes)
         jsonFile = open("../dictionnaireUtilisable/verbes.json", "w")
         jsonFile.write(jsonString)
         jsonFile.close()
 
-        # affichage du résultat
+        # affichage du resultat
         print(f' {len(listeVerbes)} verbes extraits.')
 
-# lancement du nettoyage désiré
-nettoyerVerbes()
+
+# extrait et sauvegarde les noms en supprimant ceux avec des espaces et les formes non nominales (feminin, pluriel)
+def nettoyerNoms():
+
+    # ouverture du csv
+    with open('noun.csv') as csv_noms:
+
+       # initialisation des variables
+        csv_reader = csv.reader(csv_noms, delimiter=',')
+        listeNoms = []
+        listeNomsASupprimer = []
+
+        for row in csv_reader:
+            # recuperation de tous les noms qui ne possedent pas d'espaces, qui ne sont pas tagues et dont la taille est superieure a 1 caractere
+            if " " not in row[1] and 'plural' not in row[2] and 'feminine' not in row[2] and len(row[1]) > 1:
+                listeNoms.append(row[1])
+            # recuperation de tous les noms tagues
+            if " " not in row[1] and ('plural' in row[2] or 'feminine' in row[2]) and len(row[1]) > 1:
+                listeNomsASupprimer.append(row[1])
+
+        # soustraction des mots tagues au jeu de donnees a conserver pour garder que les formes sg, et masculin
+        for nom in listeNomsASupprimer:
+            if nom in listeNoms:
+                listeNoms.remove(nom)
+
+        # sauvegarde des resultats tries dans un json
+        listeNoms.sort()
+        jsonString = json.dumps(listeNoms)
+        jsonFile = open("../dictionnaireUtilisable/noms.json", "w")
+        jsonFile.write(jsonString)
+        jsonFile.close()
+
+        # affichage du resultat
+        print(f' {len(listeNoms)} noms extraits.')
+
+
+# extrait et sauvegarde les adverbes
+def nettoyerAdverbes():
+
+    # ouverture du csv
+    with open('adv.csv') as csv_adv:
+
+       # initialisation des variables
+        csv_reader = csv.reader(csv_adv, delimiter=',')
+        listeAdv = []
+
+        # parcourt tout le csv pour trouver les adverbes
+        for row in csv_reader:
+                listeAdv.append(row[1])
+
+        # sauvegarde des resultats tries dans un json
+        listeAdv.sort()
+        jsonString = json.dumps(listeAdv)
+        jsonFile = open("../dictionnaireUtilisable/adverbes.json", "w")
+        jsonFile.write(jsonString)
+        jsonFile.close()
+
+        # affichage du resultat
+        print(f' {len(listeAdv)} adverbes extraits.')
+
+# lancement du nettoyage desire
+nettoyerAdverbes()
