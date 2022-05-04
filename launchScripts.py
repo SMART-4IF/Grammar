eedb37d3fe2325e2e0a95d9f87cf737058c3aa38
quentin-marc <quentinmarc81@gmail.com>
2022-05-04 17:10:44 +0200

    verif carotte

--- launchScripts.py
+++ launchScripts.py
@@ -3,7 +3,7 @@ import time
 
 # phrase sortie par l'IA de reconnaissance des signes
 startTime = time.time()
-phraseInitiale = ["lui", "manger", "lentement","lui"]
+phraseInitiale = ["hier", "lui", "manger", "carotte"]
 structurePhrase = StructurePhrase()
 structurePhrase = structurePhrase.traduire(phraseInitiale)
 print("temps: "+str(time.time() - startTime))
